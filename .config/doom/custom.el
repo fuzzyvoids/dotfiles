d124d757c6cbd7c9468c484af3e18c324df94460
fuzzyvoids <rob@fuzzyvoids.com>
2026-07-21 09:49:19 -0600

    Create custom.el

---
+++ .config/doom/custom.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(switch-window))
+ '(safe-local-variable-values
+   '((eval load-file
+      (expand-file-name "timekeeping-reports.el"
+       (file-name-directory (or load-file-name buffer-file-name)))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
